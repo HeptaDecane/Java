51c85717aaf19d5ea5ce422eca4180c267bafd87
NearLawiet <sooraj1999vs@gmail.com>
2020-06-30 01:21:49 +0530

    https://www.hackerrank.com/challenges/java-stdin-and-stdout-1

---
+++ StdIO-i.java
@@ -0,0 +1,17 @@
+/*
+	https://www.hackerrank.com/challenges/java-stdin-and-stdout-1
+*/
+
+import java.util.*;
+
+public class Main{
+	public static void main(String[] args) {
+		Scanner input= new Scanner(System.in);
+		int a = input.nextInt();
+		int b = input.nextInt();
+		int c = input.nextInt();
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+	}
+}
