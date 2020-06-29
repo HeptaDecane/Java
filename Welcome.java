cc31a3de0f8f2b560110c8eaab205f21823906eb
NearLawiet <sooraj1999vs@gmail.com>
2020-06-30 00:46:41 +0530

    https://www.hackerrank.com/challenges/welcome-to-java

---
+++ Welcome.java
@@ -0,0 +1,10 @@
+/*
+	https://www.hackerrank.com/challenges/welcome-to-java
+*/
+
+public class Main{
+	public static void main(String[] args) {
+		System.out.println("Hello, World.");
+		System.out.println("Hello, Java.");
+	}
+}
