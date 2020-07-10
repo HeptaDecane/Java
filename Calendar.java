81a460f9a39d1f541505ddbcfc810ba76e3f8aeb
NearLawiet <sooraj1999vs@gmail.com>
2020-07-10 14:28:53 +0530

    https://www.hackerrank.com/challenges/java-date-and-time

---
+++ Calendar.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+// Start Code
+/*
+	https://www.hackerrank.com/challenges/java-date-and-time
+*/
+import java.time.*;
+
+class Result {
+
+    public static String findDay(int month, int day, int year) {
+        LocalDate date = LocalDate.of(year,month,day);
+        DayOfWeek dayOfWeek = DayOfWeek.from(date);
+        return dayOfWeek.name();
+    }
+
+}
+// End Code
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int month = Integer.parseInt(firstMultipleInput[0]);
+
+        int day = Integer.parseInt(firstMultipleInput[1]);
+
+        int year = Integer.parseInt(firstMultipleInput[2]);
+
+        String res = Result.findDay(month, day, year);
+
+        bufferedWriter.write(res);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
