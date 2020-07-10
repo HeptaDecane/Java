3912efe32a7c972090edc40ed1f756c39c2ea2e6
NearLawiet <sooraj1999vs@gmail.com>
2020-07-10 10:41:57 +0530

    https://www.hackerrank.com/challenges/java-substring/problem

---
+++ Substring.java
@@ -0,0 +1,17 @@
+/*
+	https://www.hackerrank.com/challenges/java-substring/problem
+*/
+
+import java.util.Scanner;
+
+public class Main{
+	public static Scanner input = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		String str = input.next();
+		int start = input.nextInt();
+		int end = input.nextInt();
+
+		System.out.println(str.substring(start,end));
+	}
+}
