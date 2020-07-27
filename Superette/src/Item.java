133b51571c3c86fa3735404fda3f98358c263fb5
NearLawiet <sooraj1999vs@gmail.com>
2020-07-27 21:10:17 +0530

    Ordered Maps (LinkedHashMaps and TreeMaps)

--- Superette/src/Item.java
+++ Superette/src/Item.java
@@ -1,4 +1,4 @@
-public class Item {
+public class Item implements Comparable<Item> {
     private final String name;
     private double price;
     private int stock,reserved;
@@ -13,6 +13,78 @@ public class Item {
         this.reserved = 0;
     }
 
+    public String getName(){
+        return name;
+    }
+    public double getPrice(){
+        return price;
+    }
 
+    public int updateStock(int quantity){
+        if(quantity+stock >= 0) {
+            stock += quantity;
+            return stock;
+        }
+        return -1;
+    }
 
+    public boolean updateReserved(int quantity){
+        if(quantity>0){
+            if(stock-reserved >= quantity){
+                reserved += quantity;
+                return true;
+            }
+        }
+        if(quantity<0){
+            quantity *= -1;
+            if(reserved >= quantity){
+                reserved -= quantity;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean checkout(int quantity){
+        if(quantity<=0)
+            return false;
+        if(reserved >= quantity){
+            reserved -= quantity;
+            updateStock(-quantity);
+            return true;
+        }
+        return false;
+    }
+
+    public void available(){
+        System.out.println(stock-reserved);
+    }
+
+    @Override
+    public int compareTo(Item second) {
+        Item first = this;
+        return first.name.compareTo(second.name);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj)
+            return true;
+        if(obj instanceof Item){
+            Item first = this;
+            Item second = (Item)obj;
+            return first.name.equals(second.name);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.name.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return this.name+"  @ "+this.price+"$";
+    }
 }
--- Superette/src/Main.java
+++ Superette/src/Main.java
@@ -1,2 +1,6 @@
 public class Main {
+    public static void main(String[] args) {
+        Item pillow = new Item("Pillow",6.87,200);
+        System.out.println(pillow);
+    }
 }
