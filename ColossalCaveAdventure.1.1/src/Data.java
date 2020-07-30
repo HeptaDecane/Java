4d1806a746f35cc524c5e805753026c6cfbb18fb
NearLawiet <sooraj1999vs@gmail.com>
2020-07-30 21:47:19 +0530

    Binary File Handling (DataIOStream)

--- ColossalCaveAdventure.1.1/src/Data.java
+++ ColossalCaveAdventure.1.1/src/Data.java
@@ -2,47 +2,48 @@ import java.io.*;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.Scanner;
 
 public final class Data {
+
     public static Map<Integer,Location> readMap(){
-        System.out.print("Loading Map...");
+        System.out.print("Loading Map.");
 
         Map<Integer,Location> map = new LinkedHashMap<>();
         Map<String,Integer> exits = new HashMap<>();
         // Reading Locations
         try(
-            FileReader file = new FileReader("locations.txt");
-            BufferedReader buffer = new BufferedReader(file);
-            Scanner scanner = new Scanner(buffer);
+            FileInputStream file = new FileInputStream("locations.dat");
+            BufferedInputStream buffer = new BufferedInputStream(file);
+            DataInputStream iStream = new DataInputStream(buffer)
         ){
-            while(scanner.hasNextLine()){
-                scanner.useDelimiter(",");
-                int locationID = scanner.nextInt();
-                scanner.skip(",");
-                String description = scanner.nextLine();
+            while(true){
+                int locationID = iStream.readInt();
+                String description = iStream.readUTF();
                 map.put(locationID,new Location(locationID,description,exits));
             }
+        }catch(EOFException e){
+            System.out.print(".");
         }catch(Exception e){
-            System.out.println(e);
+            System.out.println(e.getMessage());
         }
         // Reading Exits
         try(
-            FileReader file = new FileReader("exits.txt");
-            BufferedReader buffer = new BufferedReader(file);
-            Scanner scanner = new Scanner(buffer);
+            FileInputStream file = new FileInputStream("exits.dat");
+            BufferedInputStream buffer = new BufferedInputStream(file);
+            DataInputStream iStream = new DataInputStream(buffer)
         ){
-            while(scanner.hasNextLine()){
-                String[] data = scanner.nextLine().split(",");
-                int locationID = Integer.parseInt(data[0]);
-                String direction = data[1];
-                int exitsTo = Integer.parseInt(data[2]);
+            while(true){
+                int locationID = iStream.readInt();
+                String direction = iStream.readUTF();
+                int exitsTo = iStream.readInt();
 
                 Location location = map.get(locationID);
                 location.addExit(direction,exitsTo);
             }
+        }catch(EOFException e){
+            System.out.print(".");
         }catch(Exception e){
-            System.out.println(e);
+            System.out.println(e.getMessage());
         }
 
         System.out.println("SUCCESS");
@@ -54,15 +55,13 @@ public final class Data {
 
         int currentLocationID = 1;
         try(
-            FileReader file = new FileReader("progress.txt");
-            Scanner scanner = new Scanner(file);
+            FileInputStream file = new FileInputStream("progress.dat");
+            DataInputStream iStream = new DataInputStream(file)
         ){
-            currentLocationID = scanner.nextInt();
+            currentLocationID = iStream.readInt();
         }catch (Exception e){
-            System.out.println(e);
-            currentLocationID = 1;
+            System.out.println(e.getMessage());
         }
-
         System.out.println("SUCCESS");
         return currentLocationID;
     }
@@ -70,12 +69,12 @@ public final class Data {
     public static void writeProgress(int locationID){
         System.out.print("Saving Progress...");
         try(
-            FileWriter file = new FileWriter("progress.txt");
-            BufferedWriter buffer = new BufferedWriter(file);
+            FileOutputStream file = new FileOutputStream("progress.dat");
+            DataOutputStream oStream = new DataOutputStream(file)
         ) {
-            buffer.write(Integer.toString(locationID));
-        }catch (Exception e){
-            System.out.println(e);
+            oStream.writeInt(locationID);
+        }catch(Exception e){
+            System.out.println(e.getMessage());
         }
         System.out.println("SUCCESS");
     }
