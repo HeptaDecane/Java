494a03b5b89de9082db68cafcfcbebeea121b310
NearLawiet <sooraj1999vs@gmail.com>
2020-09-01 16:26:38 +0530

    UDP Client-Server Program

---
+++ Echo-UDP/src/Server.java
@@ -0,0 +1,36 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class Server {
+    public static void main(String[] args) {
+        byte[] inData = new byte[1024];
+        byte[] outData = new byte[1024];
+        try(DatagramSocket socket = new DatagramSocket(5000)){
+            System.out.println("listening to port:5000");
+
+            while (true){
+                DatagramPacket packet = new DatagramPacket(inData,inData.length);
+                socket.receive(packet);
+
+                InetAddress address = packet.getAddress();
+                int port = packet.getPort();
+
+                String message = new String(inData,0,packet.getLength());
+                System.out.println("client: "+message);
+
+                if(message.equalsIgnoreCase("exit")){
+                    String response = "connection closed.";
+                    System.out.println(response);
+                    outData = response.getBytes();
+                    packet = new DatagramPacket(outData,outData.length,address,port);
+                    socket.send(packet);
+                    break;
+                }
+            }
+
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
