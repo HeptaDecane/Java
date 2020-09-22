813cef96b93e02f2eda96b9368b921bec2885e84
NearLawiet <sooraj1999vs@gmail.com>
2020-09-22 09:54:33 +0530

    multi-threaded server

--- MultiEcho/src/Server.java
+++ MultiEcho/src/Server.java
@@ -1,14 +1,17 @@
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Server {
     public static void main(String[] args) {
         try(ServerSocket serverSocket = new ServerSocket(5000)){
             System.out.println("listening to port:5000");
+            ExecutorService service = Executors.newCachedThreadPool();
             while (true){
                 Socket clientSocket = serverSocket.accept();
                 ServerThread serverThread = new ServerThread(clientSocket);
-                serverThread.start();
+                service.execute(serverThread);
             }
         } catch (Exception e){
             System.out.println("error: "+e.getMessage());
